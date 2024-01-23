4ef558c14885729000bf3eb82b51ed58ef964212
Pedro Ferreira <116006000+Fipura@users.noreply.github.com>
2024-01-23 14:35:04 +0000

    fix

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
-require('dotenv').config();
+require("dotenv").config();
 
 const mongoString = process.env.DATABASE_URL;
 
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 
-const routes = require('./routes/rotas');
+const routes = require("./routes/rotas");
 
 const app = express();
 
-app.use(express.json);
+app.use(express.json());
 app.use(routes);
 
-app.listen(3000, () => {
-    console.log('Server is running');
-})
+app.listen(8081, () => {
+  console.log("Server is running");
+});
 
 mongoose.connect(mongoString);
 
 const database = mongoose.connection;
 
-database.on('erro', (erro) => {
-    console.log(erro);
-}) 
+database.on("error", (error) => {
+  console.log(error);
+});
 
-database.once('connected', () => {
-    console.log('Database connected');
-})
+database.once("connected", () => {
+  console.log("Database connected");
+});
 
 app.get("/", (req, res) => {
-    res.json({ message: "Welcome to bezkoder application." });
-  });
+  res.json({ message: "Welcome to bezkoder application." });
+});
